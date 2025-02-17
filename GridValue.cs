54d579225b0d61d186c725136f57dd8cd328800b
Wensley Mercedes <163855103+tech23242627@users.noreply.github.com>
2025-02-17 01:19:33 -0400

    Add files via upload

---
+++ GridValue.cs
@@ -0,0 +1,10 @@
+﻿namespace Snake
+{
+    public enum GridValue
+    {
+        Empty,
+        Snake,
+        Food,
+        Outside        
+    }
+}
---
+++ Images.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Snake
+{
+    public static class Images
+    {
+        public static readonly ImageSource Empty = LoadImage("Emty.png");
+        public static readonly ImageSource Body = LoadImage("Body.png");
+        public static readonly ImageSource Head = LoadImage("Head.png");
+        public static readonly ImageSource Food = LoadImage("Food.png");
+        public static readonly ImageSource DeadBody = LoadImage("DeadBody.png");
+        public static readonly ImageSource DeadHead = LoadImage("DeadHead.png");
+
+        private static ImageSource LoadImage(string filename)
+        {
+            return new BitmapImage(new Uri($"Assets/{filename}", UriKind.Relative));
+        }
+    }
+}
